cf25f075eec5e7303261ce562a219b806854563b
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-06-03 19:06:40 -0600

    Session 4

---
+++ May AN 2020/Assets/Scripts/SimhandMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimhandMovement : MonoBehaviour
+{
+    public Transform m_hand;
+    public float m_moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            m_hand.Translate(Vector3.forward * Time.deltaTime * m_moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_hand.Translate(Vector3.back * Time.deltaTime * m_moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_hand.Translate(Vector3.left * Time.deltaTime * m_moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_hand.Translate(Vector3.right * Time.deltaTime * m_moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_hand.Translate(Vector3.up * Time.deltaTime * m_moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            m_hand.Translate(Vector3.down * Time.deltaTime * m_moveSpeed);
+        }
+
+        m_hand.Rotate(Input.GetAxis("Mouse X") * Vector3.up, Space.World);
+        m_hand.Rotate(Input.GetAxis("Mouse Y") * Vector3.left);
+    }
+}
--- May AN 2020/Assets/Scripts/Test.cs
+++ May AN 2020/Assets/Scripts/Test.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
+    public Text m_myText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_myText.text = "Hi\nhello";
     }
 
     // Update is called once per frame
