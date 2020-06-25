0f339ed9fa91a3918061a4c0bf496f35313f4907
Usman <usman@circuitstream.com>
2020-06-24 18:46:55 -0600

    Session 9

---
+++ May AN 2020/Assets/Scripts/Simhand/SimGrab.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimGrab : MonoBehaviour
+{
+    public Animator m_anim;
+
+    private GameObject m_touchingObject;
+    private GameObject m_heldObject;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Interactable")
+        {
+            m_touchingObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_touchingObject = null;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            m_anim.SetBool("isGrabbing", true);
+            if(m_touchingObject)
+            {
+                Grab();
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            m_anim.SetBool("isGrabbing", false);
+            if(m_heldObject)
+            {
+                Release();
+            }
+        }
+    }
+
+    void Grab()
+    {
+        m_heldObject = m_touchingObject;
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        m_heldObject.transform.SetParent(transform);
+    }
+
+    void Release()
+    {
+        m_heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        m_heldObject.transform.SetParent(null);
+        m_heldObject = null;
+    }
+}
