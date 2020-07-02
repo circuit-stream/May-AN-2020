58eb059ca90534e69a6945aa6907369a4d525bff
Usman <usman@circuitstream.com>
2020-07-01 18:23:50 -0600

    Session 10

---
+++ May AN 2020/Assets/Scripts/AIFollower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIFollower : MonoBehaviour
+{
+    public float m_agroDistance = 10f;
+    public float m_turnSpeed = 4f;
+
+    public Transform m_target;
+
+    private NavMeshAgent m_agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+        m_target = PlayerSingleton.s_instance.m_player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, m_target.position) <m_agroDistance)
+        {
+            m_agent.SetDestination(m_target.position);
+
+            Vector3 dir = m_target.position - transform.position;
+            dir.Normalize();
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(dir.x, 0, dir.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, m_turnSpeed * Time.deltaTime);
+        }
+        else
+        {
+            m_agent.SetDestination(transform.position);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, m_agroDistance);
+    }
+}
---
+++ May AN 2020/Assets/Scripts/PlayerSingleton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSingleton : MonoBehaviour
+{
+    public static PlayerSingleton s_instance;
+    public Transform m_player;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        s_instance = this;
+        DontDestroyOnLoad(gameObject);
+        m_player = transform;
+    }
+}
