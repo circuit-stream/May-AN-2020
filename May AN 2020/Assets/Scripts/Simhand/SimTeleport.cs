11960932f5f75bdb47dc9b1b2696cd5faefca189
Usman <usman@circuitstream.com>
2020-07-06 18:13:02 -0600

    Session 11

---
+++ May AN 2020/Assets/Scripts/Simhand/SimTeleport.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimTeleport : MonoBehaviour
+{
+    public LayerMask m_teleportableLayer;
+    public LineRenderer m_teleportLine;
+    private RaycastHit m_hit;
+    private bool m_canTeleport;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.T))
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out m_hit, Mathf.Infinity, m_teleportableLayer))
+            {
+                m_teleportLine.SetPosition(0, transform.position);
+                m_teleportLine.SetPosition(1, m_hit.point);
+                m_teleportLine.enabled = true;
+                m_canTeleport = true;
+            }
+            else
+            {
+                m_teleportLine.enabled = false;
+                m_canTeleport = false;
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.T))
+        {
+            m_teleportLine.enabled = false;
+            if(m_canTeleport)
+            {
+                transform.position = m_hit.point;
+            }
+        }
+    }
+}
---
+++ May AN 2020/Assets/Scripts/VR/VRTeleport.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTeleport : MonoBehaviour
+{
+    public string m_teleportButtonName;
+    public Transform m_VRPlayer;
+    public LineRenderer m_teleportLine;
+    private RaycastHit m_hit;
+    private bool m_canTeleport;
+
+    void Update()
+    {
+        if (Input.GetButton(m_teleportButtonName))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out m_hit))
+            {
+                m_teleportLine.SetPosition(0, transform.position);
+                m_teleportLine.SetPosition(1, m_hit.point);
+                m_teleportLine.enabled = true;
+                m_canTeleport = true;
+            }
+            else
+            {
+                m_teleportLine.enabled = false;
+                m_canTeleport = false;
+            }
+        }
+        else if (Input.GetButtonUp(m_teleportButtonName))
+        {
+            m_teleportLine.enabled = false;
+            if (m_canTeleport)
+            {
+                m_VRPlayer.position = m_hit.point;
+            }
+        }
+    }
+}
