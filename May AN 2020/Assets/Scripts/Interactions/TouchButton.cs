805bf579e5f5f7204ca259f49c0c0750d93e6930
Usman <usman@circuitstream.com>
2020-07-13 19:34:12 -0600

    Session 12

---
+++ May AN 2020/Assets/Scripts/Interactions/TouchButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TouchButton : MonoBehaviour
+{
+    public Transform m_buttonMesh;
+    public Transform m_upTransform;
+    public Transform m_downTransform;
+    public UnityEvent m_buttonPressed;
+    public UnityEvent m_buttonReleased;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_buttonMesh.position = m_downTransform.position;
+            m_buttonPressed.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            m_buttonMesh.position = m_upTransform.position;
+            m_buttonReleased.Invoke();
+        }
+    }
+}
